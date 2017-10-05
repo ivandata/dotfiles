a7680d890d6d41542e18c5c40fb302baccf9ff3b
Ivandata <ivandata@users.noreply.github.com>
2017-10-05 13:44:52 +0300

    Move link function, command_exists function added, print_in_blue function changed

--- utils.sh
+++ utils.sh
@@ -13,16 +13,20 @@ print_in_yellow() {
     printf "$(tput setaf 003) $1 \n"
 }
 
-print_in_blue() {
+print_in_white() {
     printf "$(tput setaf 004) $1 \n"
 }
 
 # Messages and questions
 ask_question() {
     printf "\n"
-    print_in_blue "[?] $@"
-    read -p "Continue? (y/n) " -n 1
+    print_in_yellow "[?] $@"
+    read -p "Continue? (y/n) " -n 1;
     printf "\e[mR\n"
+}
+
+header_message() {
+    print_in_white "$1\n"
 }
 
 warning_message() {
@@ -40,6 +44,20 @@ error_message() {
 # Test whether the result of an 'ask' is a confirmation
 is_confirmed() {
     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+      return 0
+    fi
+    return 1
+}
+
+# Force create/replace the symlink.
+link() {
+    ln -fs "${1}/${2}" "${HOME}/${3}"
+}
+
+# Test whether a command exists
+# $1 - cmd to test
+command_exists() {
+    if [ $(type -P $1) ]; then
       return 0
     fi
     return 1
