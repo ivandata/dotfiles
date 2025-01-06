2197925ee6e2074c0db11643453f3f2a0c6ae4bb
Ivandata <ivandata@users.noreply.github.com>
2025-01-06 17:49:20 +0100

    feat: Improve utils

--- utils.sh
+++ utils.sh
@@ -2,79 +2,82 @@
 
 # Print in colors
 print_in_red() {
-    printf "$(tput setaf 001)$1"
+    printf "\033[31m%s\033[0m" "$1"
 }
 
 print_in_green() {
-    printf "$(tput setaf 002)$1"
+    printf "\033[32m%s\033[0m" "$1"
 }
 
 print_in_yellow() {
-    printf "$(tput setaf 003)$1"
+    printf "\033[33m%s\033[0m" "$1"
 }
 
 print_in_white() {
-    printf "$(tput setaf 007)$1"
+    printf "\033[37m%s\033[0m" "$1"
 }
 
 # Messages and questions
 ask_question() {
-    printf "\n"
-    read -p "$(tput setaf 003)[?] $(tput sgr0)$@ Continue? (y/n) " -n 1;
-    printf "\e[mR\n"
+    printf "\\n"
+    read -r -p "$(printf "\033[33m[?]\033[0m %s Continue? (y/n) " "$@")" -n 1 REPLY
+    printf "\\n"
 }
 
 header_message() {
-    print_in_white "[➜] $(tput sgr0)$1 \n"
+    printf "\033[37m[➜] %s\033[0m\\n" "$1"
 }
 
 warning_message() {
-    print_in_yellow "[!] $(tput sgr0)$1 \n";
+    printf "\033[33m[!] %s\033[0m\\n" "$1"
 }
 
 success_message() {
-    print_in_green "[✔] $(tput sgr0)$1 \n";
+    printf "\033[32m[✔] %s\033[0m\\n" "$1"
 }
 
 error_message() {
-    print_in_red "[✖] $(tput sgr0)$1 \n";
+    printf "\033[31m[✖] %s\033[0m\\n" "$1"
 }
 
 # Test whether the result of an 'ask' is a confirmation
 is_confirmed() {
-
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-      return 0
-    fi
-
-    return 1
+    [[ "$REPLY" =~ ^[Yy]$ ]]
 }
 
-# Force create/replace the symlink.
+# Force create/replace the symlink
 link() {
-    ln -fs "${1}/${2}" "${HOME}/${3}"
+    ln -fs "$1/$2" "$HOME/$3"
+    if [ $? -eq 0 ]; then
+        success_message "Symlink created for $2"
+    else
+        error_message "Failed to create symlink for $2"
+        exit 1
+    fi
 }
 
 # Test whether a command exists
 # $1 - cmd to test
 command_exists() {
-    type -P "$1" &>/dev/null
+    command -v "$1" &>/dev/null
 }
 
-# Ask for the administrator password upfront.
+# Ensure a command exists or exit
+require_command() {
+    if ! command_exists "$1"; then
+        error_message "Required command '$1' not found. Please install it and retry."
+        exit 1
+    fi
+}
+
+# Ask for the administrator password upfront
 ask_for_sudo() {
+    sudo -v &>/dev/null
 
-    sudo -v &> /dev/null
-
-    # Update existing `sudo` time stamp
-    # until this script has finished.
-    #
-    # https://gist.github.com/cowboy/3118588
-
+    # Update existing `sudo` timestamp until this script finishes
     while true; do
         sudo -n true
         sleep 60
         kill -0 "$$" || exit
-    done &> /dev/null &
-
+    done &>/dev/null &
 }
